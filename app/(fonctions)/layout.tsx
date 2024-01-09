5a770d9ea24f98a7b918253ec666107437afe797
amadoucodeur <amadoucodeur@gmail.com>
2024-01-09 10:54:04 +0000

    le logo

--- app/(fonctions)/layout.tsx
+++ app/(fonctions)/layout.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,9 +7,16 @@ export default function RootLayout({
 }) {
   return (
     <main className=" fixed grid grid-cols-[250px,1fr] w-full">
-      <ul className="flex flex-col p-2 bg-black">
-        <li className="m-2 font-bold text-center text-2xl">
-          <a href="/">Trow</a>
+      <ul className="flex flex-col p-2 bg-black ">
+        <li className="m-2 font-bold text-center text-2xl flex justify-center items-center flex-col w-full">
+          <a href="/">
+            <Image
+              src={"/logo.png"}
+              width={100}
+              height={100}
+              alt="le logo de tomorrow"
+            />
+          </a>
         </li>
         <li className="gap-1 mt-2 hover:bg-gray-800 bg-gray-900 p-2 rounded transition duration-300">
           <a href="/correcteur">Correcteur</a>
